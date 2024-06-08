6806da451c19612b40cd35f9a5fac1aa7a7bf432
Prosper Leibundgut <leiu@zhaw.ch>
2024-06-08 13:36:09 +0200

    Minimal working example on the TMS570LS1224 target. Successfully stepped through the entire program with the proprietary Texas Instruments debugger.

--- loop/src/sys_core.s
+++ loop/src/sys_core.s
@@ -11,7 +11,7 @@
 
 
 _core_init_registers_:
-    mov r0, lr
+    /* After reset, the CPU is in the Supervisor mode (M = 10011) */
     mov r1, #0x0000
     mov r2, #0x0000
     mov r3, #0x0000
@@ -21,10 +21,10 @@ _core_init_registers_:
     mov r7, #0x0000
     mov r8, #0x0000
     mov r9, #0x0000
-    mov r10, #0x000
-    mov r11, #0x000
-    mov r12, #0x000
-    mov r13, #0x000
+    mov r10, #0x0000
+    mov r11, #0x0000
+    mov r12, #0x0000
+    mov r13, #0x0000
     mrs r1, cpsr
     msr spsr_cxsf, r1
     /* Switch to FIQ mode (M = 10001) */
@@ -52,9 +52,43 @@ _core_init_registers_:
     mov lr, r0
     mrs r1,cpsr
     msr spsr_cxsf, r1
-    /* Switch back to Supervisor Mode (M = 10011) */
-    cps #19
+    /* Switch to System Mode ( Shares User Mode registers ) (M = 11111) */
+    cps #31
+    mov lr, r0
+    mrs r1,cpsr
+    msr spsr_cxsf, r1
 
+    mrc   p15,     #0x00,      r2,       c1, c0, #0x02
+    orr   r2,      r2,         #0xF00000
+    mcr   p15,     #0x00,      r2,       c1, c0, #0x02
+    mov   r2,      #0x40000000
+    fmxr  fpexc,   r2
+
+    fmdrr d0,         r1,     r1
+    fmdrr d1,         r1,     r1
+    fmdrr d2,         r1,     r1
+    fmdrr d3,         r1,     r1
+    fmdrr d4,         r1,     r1
+    fmdrr d5,         r1,     r1
+    fmdrr d6,         r1,     r1
+    fmdrr d7,         r1,     r1
+    fmdrr d8,         r1,     r1
+    fmdrr d9,         r1,     r1
+    fmdrr d10,        r1,     r1
+    fmdrr d11,        r1,     r1
+    fmdrr d12,        r1,     r1
+    fmdrr d13,        r1,     r1
+    fmdrr d14,        r1,     r1
+    fmdrr d15,        r1,     r1
+    bl    next1
+
+next1:
+    bl    next2
+next2:
+    bl    next3
+next3:
+    bl    next4
+next4:
     b    _core_init_stack_pointer_
 
 
--- misc/readme.txt
+++ misc/readme.txt
@@ -2,3 +2,9 @@
 The TMS570LS1224.ccxml can be obtained from the TI uniflash utility or
 from the TI code composer studio, somewhere in the project settings.
 
+The testBoard.dat can be obtained from the TI code composer studio.
+I.e.: Connect a board and detect / verify the connection with the
+fancy GUI, e.g. within the procedure / dialog when creating a new
+project. The path to testBoard.dat is displayed while the connection
+is successfully verified.
+
